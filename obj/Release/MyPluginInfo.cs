404046d1cea52cde55f9670cf9433fe11de64e88
Yascob99 <yascob09@gmail.com>
2026-02-25 07:55:07 -0700

    Removed accidentally pushed build

--- obj/Release/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BluePrinceLuckHUD
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "BluePrinceLuckHUD";
-        public const string PLUGIN_NAME = "A Luck Hud for Blue Prince";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
